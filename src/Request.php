c2b31da4273d94bcd0eee631c04a3e64b147546a
rtartaglia <tartaglia.riccardo@gmail.com>
2016-03-01 10:37:55 +0100

    boom

--- src/Request.php
+++ src/Request.php
@@ -21,7 +21,7 @@ class Request {
             exit;
         }
         
-        $this->message = isset($update['message']) ? $update['message'] : "";
+        $this->message = isset($this->update['message']) ? $this->update['message'] : "";
         $this->messageId = isset($this->message['message_id']) ? $this->message['message_id'] : "";
         $this->chatId = isset($this->message['chat']['id']) ? $this->message['chat']['id'] : "";
         $this->senderId = isset($this->message['chat']['id']) ? $this->message['chat']['id'] : "";
