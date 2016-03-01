b1858c90b627f72c166b92739bf076111996f583
rtartaglia <tartaglia.riccardo@gmail.com>
2016-03-01 09:06:26 +0100

    boom

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://provarikbot.herokuapp.com/execute.php';
+$BOT_TOKEN = '161245058:AAGFsxRNURmEbwpOONc4_89NeNfrlPHwR0I';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
