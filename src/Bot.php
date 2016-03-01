577bf39c98ee7c6f1e352915f97020ec2ab939cf
rtartaglia <tartaglia.riccardo@gmail.com>
2016-03-01 11:39:41 +0100

    boom

--- src/Bot.php
+++ src/Bot.php
@@ -7,6 +7,7 @@
 class Bot {
 
     private $REQUEST;
+    private $STATE = 0;
 
     function __construct() {
         $this->REQUEST = new Request();
@@ -23,8 +24,20 @@ class Bot {
                 $mex = "Arrivederci!";
                 $this->apiSendMessage($mex);
                 break;
+            case "/search":
+                $this->STATE = 1;
+                $mex = "Digita le parole chiavi delle gif";
+                $this->apiSendMessage($mex);
+                break;
 
             default:
+                if ($this->STATE === 1) {
+                    $this->giphyApi();
+                    $this->STATE = 0;
+                } else {
+                    $mex = "Sono un po' stupido, ripeti il comando :)";
+                    $this->apiSendMessage($mex);
+                }
                 break;
         }
     }
@@ -37,6 +50,11 @@ class Bot {
         header("Content-Type: application/json");
         $params["method"] = "sendMessage";
         echo json_encode($params);
+    }
+
+    function giphyApi() {
+        $mex = file_get_contents('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC');
+        $this->apiSendMessage($mex);
     }
 
 }
